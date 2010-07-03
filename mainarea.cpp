e6618b4a14a1c2cb84427d09ef61ed7b899212c4
Stefan Majewsky <majewsky@gmx.net>
2010-07-03 15:41:16 +0000

    SVN_SILENT krazy c++/doublequote_chars
    svn path=/trunk/KDE/kdegames/kollision/; revision=1145629

--- mainarea.cpp
+++ mainarea.cpp
@@ -124,7 +124,7 @@ Animation* MainArea::writeMessage(const QString& text)
 Animation* MainArea::writeText(const QString& text, bool fade)
 {
     m_welcome_msg.clear();
-    foreach (const QString &line, text.split("\n")) {
+    foreach (const QString &line, text.split('\n')) {
         m_welcome_msg.append(
             KSharedPtr<Message>(new Message(line, m_msg_font, m_size)));
     }
