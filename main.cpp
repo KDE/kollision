6b3dbb9e296fb5479b983032a9871e8096d7e0a0
Montel Laurent <montel@kde.org>
2015-01-15 13:43:19 +0100

    Add dbus support

--- main.cpp
+++ main.cpp
@@ -14,12 +14,14 @@
 #include <KLocalizedString>
 #include <QCommandLineParser>
 #include <kdelibs4configmigrator.h>
-
+#include <KDBusService>
 #include "mainwindow.h"
 
 
 int main(int argc, char *argv[])
 {
+    QApplication app(argc, argv);
+
     KAboutData aboutData("kollision", i18n("Kollision"),
                          "0.1", i18n("KDE collision game"), KAboutLicense::GPL,
                          i18n("(c) 2007 Paolo Capriotti"), "http://games.kde.org/kollision");
@@ -28,7 +30,6 @@ int main(int argc, char *argv[])
     aboutData.addCredit(i18n("Matteo Guarnieri"), i18n("Original idea"));
     aboutData.addCredit(i18n("Brian Croom"), i18n("Port to use KGameRenderer"));
 
-    QApplication app(argc, argv);
 
     Kdelibs4ConfigMigrator migrate(QStringLiteral("kollision"));
     migrate.setConfigFiles(QStringList() << QStringLiteral("kollisionrc"));
@@ -43,7 +44,7 @@ int main(int argc, char *argv[])
     aboutData.setupCommandLine(&parser);
     parser.process(app);
     aboutData.processCommandLine(&parser);
-
+    KDBusService service;
 
     MainWindow* window = new MainWindow;
     window->show();
