5e67638524c67df993fc6765d906d80828a40803
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-31 20:31:36 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- src/mainarea.h
+++ src/mainarea.h
@@ -1,11 +1,8 @@
 /*
-  Copyright (c) 2007 Paolo Capriotti <p.capriotti@gmail.com>
-  Copyright (c) 2010 Brian Croom <brian.s.croom@gmail.com>
+    SPDX-FileCopyrightText: 2007 Paolo Capriotti <p.capriotti@gmail.com>
+    SPDX-FileCopyrightText: 2010 Brian Croom <brian.s.croom@gmail.com>
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #ifndef MAINAREA_H
